2abd07c97b8645ee50c15015fd5717c2815a3096
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-06-26 00:34:43 +0200

    hello world

---
+++ src/view/view.java
@@ -0,0 +1,33 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Kramok on 25/6/2015.
+ */
+public class view {
+
+    private static void createAndShowGUI() {
+        //Create a window
+        JFrame frame = new JFrame("SwingFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add label
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+
+}
