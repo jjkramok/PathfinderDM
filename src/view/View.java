bd247d815d1e2663fdd89cd7191134d6d04a144d
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-06-28 21:08:36 +0200

    Added a layout to view and moved dicehandler to model

--- src/view/View.java
+++ src/view/View.java
@@ -1,10 +1,11 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
- + * Created by Kramok on 25/6/2015.
- + */
+* Created by Kramok on 25/6/2015.
+*/
 public class View {
 
     private static void createAndShowGUI() {
@@ -12,9 +13,24 @@ public class View {
         JFrame frame = new JFrame("SwingFrame");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //Create a panel
+        JPanel panel = new JPanel(new BorderLayout());
+
+        //Creating a layout
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new FlowLayout());
+
+
+
         //Add label
         JLabel label = new JLabel("Hello World");
         frame.getContentPane().add(label);
+
+        //Add button
+        JButton button = new JButton("roll die");
+        frame.getContentPane().add(button);
+
+        contentPane.add(button, BorderLayout.PAGE_START);
 
         //Display the window
         frame.pack();
